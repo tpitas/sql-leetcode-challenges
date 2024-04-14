22823f3190d134629f38a50235bf92eee0074d06
thiernobarry <tmabarry@yahoo.com>
2024-04-13 20:51:39 -0400

    Added solution to to deleting duplicate emails

--- easy/196-Delete-duplicate-emails.sql
+++ easy/196-Delete-duplicate-emails.sql
@@ -19,3 +19,29 @@ For example, after running your query, the above Person table should have the fo
 Note:
 Your output is the whole Person table after executing your sql. Use delete statement.
 */
+
+-- create table person
+create table person
+(
+    id int,
+    email varchar(55)
+);
+
+-- insert records into person
+insert into person (id, email) values
+(1, 'john@example.com'),
+(2, 'bob@example.com '),
+(3, 'john@example.com');
+
+-- solution
+with cte as
+(
+    select *,
+    row_number() over(partition by email order by id) rn
+    from person
+)
+delete from person
+where id in (select cte.id from cte where cte.rn > 1);
+
+-- verify soltution
+select * from person;
