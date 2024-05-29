6bc5c3aeaef4ad14250e5250f9fcb8fafaa6a0a2
thiernobarry <tmabarry@yahoo.com>
2024-05-28 22:12:05 -0400

    Added solution

--- hard/0601-Human-traffic-of-stadium.sql
+++ hard/0601-Human-traffic-of-stadium.sql
@@ -45,3 +45,39 @@ The four rows with ids 5, 6, 7, and 8 have consecutive ids and each of them has 
 The rows with ids 2 and 3 are not included because we need at least three consecutive ids.
 */
 
+-- solution
+
+create table stadium (
+  id  int ,
+  visit_date  date,
+  people int 
+);
+
+insert into stadium (id, visit_date, people) values
+(1 , '2017-01-01' , 10  ),
+(2 , '2017-01-02' , 109 ),
+(3 , '2017-01-03' , 150 ),
+(4 , '2017-01-04' , 99  ),
+(5 , '2017-01-05' , 145 ),
+(6 , '2017-01-06' , 1455),
+(7 , '2017-01-07' , 199 ),
+(8 , '2017-01-09' , 188 );
+
+with cte as (
+     select id, 
+     visit_date,
+     people,
+     id - row_number() over(order by visit_date) as dates
+     from stadium
+     where people >= 100) 
+            
+select cte.id, 
+       cte.visit_date,
+       cte.people
+from cte
+left join (
+       select dates, count(*) as total
+       from cte
+       group by dates) as b
+using (dates)
+where b.total > 2;
