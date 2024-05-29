30d27d3bb01d08466b26abf38cec96a3b8401fd1
thiernobarry <tmabarry@yahoo.com>
2024-05-28 21:50:43 -0400

    Add solution of Q569

--- hard/569-Median-employee-salary.sql
+++ hard/569-Median-employee-salary.sql
@@ -39,3 +39,38 @@ Bonus points if you can solve it without using any built-in SQL functions.
 +-----+------------+--------+
 */
 
+-- solution
+
+create table employee 
+(
+	id int ,
+	company  varchar(55),
+	salary  int
+);
+
+insert into employee(id,company, salary) values
+(1 , 'A' , 2341 ),
+(2 , 'A' , 341  ),
+(3 , 'A' , 15   ),
+(4 , 'A' , 15314),
+(5 , 'A' , 451  ),
+(6 , 'A' , 513  ),
+(7 , 'B' , 15   ),
+(8 , 'B' , 13   ),
+(9 , 'B' , 1154 ),
+(10, 'B' , 1345 ),
+(11, 'B' , 1221 ),
+(12, 'B' , 234  ),
+(13, 'C' , 2345 ),
+(14, 'C' , 2645 ),
+(15, 'C' , 2645 ),
+(16, 'C' , 2652 ),
+(17, 'C' , 65   );
+
+select id, company, salary
+from
+	(select *, 
+	row_number() over(partition by company order by salary) as rn,
+	count(*) over(partition by company) as cnt
+	from employee) as emp
+where rn between cnt / 2 and cnt / 2+1;
